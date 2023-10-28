9662868b115813184cf38ad62eadae438c49a9aa
Teboho Mphure <tebohomphure@gmail.com>
2023-10-28 18:10:16 +0200

    update env variable

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import logging
 
 consumer_key = os.environ.get("CONSUMER_KEY")
-consumer_secret = os.environ.get("CONSUMER_SECRET")
+consumer_secret = os.environ.get("CONSUMER_KEY_SECRET")
 access_token = os.environ.get("ACCESS_TOKEN")
 access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
